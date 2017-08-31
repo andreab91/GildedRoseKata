e81553fbc5b09d80372e8df0695d5973b14d676a
Andrea Bergamini <andrea.bergamini@xpeppers.com>
2017-08-31 16:32:30 +0200

    extracts backstagepass logic

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -26,23 +26,35 @@ class GildedRose {
                 continue;
             }
 
-            if (items[i].name.equals("Aged Brie") || items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                    if (items[i].sellIn < 11) {
+                        if (items[i].quality < 50) {
+                            items[i].quality = items[i].quality + 1;
                         }
                     }
+
+                    if (items[i].sellIn < 6) {
+                        if (items[i].quality < 50) {
+                            items[i].quality = items[i].quality + 1;
+                        }
+                    }
+                }
+
+                items[i].sellIn = items[i].sellIn - 1;
+
+                if (items[i].sellIn < 0) {
+                    items[i].quality = 0;
+                }
+
+                continue;
+            }
+
+            if (items[i].name.equals("Aged Brie")) {
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
                 }
             } else {
                 if (items[i].quality > 0) {
@@ -59,12 +71,8 @@ class GildedRose {
                         items[i].quality = items[i].quality + 1;
                     }
                 } else {
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        items[i].quality = 0;
-                    } else {
-                        if (items[i].quality > 0) {
-                            items[i].quality = items[i].quality - 1;
-                        }
+                    if (items[i].quality > 0) {
+                        items[i].quality = items[i].quality - 1;
                     }
                 }
             }
