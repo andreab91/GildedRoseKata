d3ee3f65cb74a769370a71cf0a168f1dda08768c
Andrea Bergamini <andrea.bergamini@xpeppers.com>
2017-08-31 15:18:55 +0200

    adds test for quality degree twice as fast

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,10 +8,22 @@ public class GildedRoseTest {
 
     @Test
     public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[] { new Item("fixme", 0, 0) };
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
+
         assertEquals("fixme", app.items[0].name);
     }
 
+    @Test
+    public void quality_degrades_twice_as_fast_when_sell_date_passed() throws Exception {
+        Item[] items = new Item[] { new Item("A standard item", -1, 2) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(-2, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);
+    }
 }
